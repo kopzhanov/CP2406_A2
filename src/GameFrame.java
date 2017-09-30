f79a5824a16fc057b570fcd55b16292043fe49aa
kopzhanov <alikhan.kopzhanov@my.jcu.edu.au>
2017-09-30 17:19:11 +0800

    Implemented functions: choose category, last played card + minor updates

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,50 +7,68 @@ import java.awt.event.ActionListener;
 public class GameFrame extends JFrame implements ActionListener {
 
     final ImageIcon CARDBACK_IMAGE = getScaledImage(new ImageIcon("images/Slide65.jpg"));
-    final Dimension CARD_SIZE_DIMENSION = new Dimension(200,300);
-    final int DEFAULT_COLUMNS = 4;
+    final Dimension CARD_SIZE_DIMENSION = new Dimension(200, 300);
+    final int DEFAULT_COLUMNS = 3;
 
-    JPanel playerAmountChoice = new JPanel(new BorderLayout());
-    JPanel table = new JPanel(new BorderLayout());
-    JPanel categoryChoice = new JPanel(new GridLayout(5,1));
-    JLabel status = new JLabel("Mineral Supertrumps", SwingConstants.CENTER);
+    JPanel playerAmountChoice;
+    JPanel hand;
+    JPanel table;
+    JPanel categoryChoice;
+    JLabel lastPlayedCard;
+    JLabel deck;
+    JLabel status;
+    JLabel newRoundNotification;
 
     public GameFrame(String title) throws HeadlessException {
         super(title);
 
+        table = new JPanel(new BorderLayout());
+
+        newRoundNotification = new JLabel("", SwingConstants.CENTER);
+
+        lastPlayedCard = new JLabel("", SwingConstants.CENTER);
+        lastPlayedCard.setPreferredSize(new Dimension(200, 320));
+        lastPlayedCard.setText("Last played card");
+        lastPlayedCard.setHorizontalTextPosition(JLabel.CENTER);
+        lastPlayedCard.setVerticalTextPosition(JLabel.BOTTOM);
+
+        status = new JLabel("Mineral Supertrumps", SwingConstants.CENTER);
         status.setOpaque(true);
         status.setBackground(new Color(230, 219, 224));
-        JLabel deck = new JLabel(CARDBACK_IMAGE);
+        deck = new JLabel(CARDBACK_IMAGE);
         deck.setText("Cards left: " + MineralSupertrumps.deck.size());
         deck.setHorizontalTextPosition(JLabel.CENTER);
         deck.setVerticalTextPosition(JLabel.BOTTOM);
-        deck.setPreferredSize(new Dimension(200,320));
+        deck.setPreferredSize(new Dimension(200, 320));
 
-        table.setPreferredSize(new Dimension(300,400));
+
+        table.add(newRoundNotification, "North");
         table.add(status, "South");
         table.add(deck);
-        table.setBorder(new EmptyBorder(10,10,10,10));
+        table.setBorder(new EmptyBorder(10, 10, 10, 10));
 
-        add(table,BorderLayout.EAST);
+        add(table, BorderLayout.EAST);
 
-        setBackground( new Color(239,237,227) );
+        setBackground(new Color(239, 237, 227));
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocation(120,70);
+        setLocation(120, 70);
         setResizable(false);
         setVisible(true);
     }
 
-    void playerAmountPanel(){
+    void playerAmountPanel() {
+        playerAmountChoice = new JPanel(new BorderLayout());
+
         JLabel prompt = new JLabel("How many players?");
         JButton three = new JButton("3");
         JButton four = new JButton("4");
         JButton five = new JButton("5");
 
         prompt.setFont(new Font("Serif", Font.PLAIN, 20));
-        three.setPreferredSize(new Dimension(200,200));
-        four.setPreferredSize(new Dimension(200,200));
-        five.setPreferredSize(new Dimension(200,200));
+        three.setPreferredSize(new Dimension(200, 200));
+        four.setPreferredSize(new Dimension(200, 200));
+        five.setPreferredSize(new Dimension(200, 200));
 
         three.addActionListener(this);
         four.addActionListener(this);
@@ -63,17 +80,54 @@ public class GameFrame extends JFrame implements ActionListener {
         playerAmountChoice.add(five, "East");
 
         add(playerAmountChoice, BorderLayout.CENTER);
-        //setPreferredSize(new Dimension(1000,300));
         pack();
         validate();
     }
 
-    void categoryPanel(Card card){
+    void categoryPanel(Card card) {
+        categoryChoice = new JPanel(new GridLayout(5, 1));
+
         JButton hardness = new JButton("Hardness: " + card.getHardness());
         JButton gravity = new JButton("Specific Gravity: " + card.getGravity());
         JButton cleavage = new JButton("Cleavage: " + card.getCleavage());
         JButton abundance = new JButton("Crustal Abundance: " + card.getAbundance());
         JButton ecoValue = new JButton("Economic Value: " + card.getEcoValue());
+
+        hardness.addActionListener(e -> {
+            MineralSupertrumps.firstTurn = false;
+            MineralSupertrumps.category = 1;
+            remove(categoryChoice);
+            pack();
+            MineralSupertrumps.turnFinished();
+        });
+        gravity.addActionListener(e -> {
+            MineralSupertrumps.firstTurn = false;
+            MineralSupertrumps.category = 2;
+            remove(categoryChoice);
+            pack();
+            MineralSupertrumps.turnFinished();
+        });
+        cleavage.addActionListener(e -> {
+            MineralSupertrumps.firstTurn = false;
+            MineralSupertrumps.category = 3;
+            remove(categoryChoice);
+            pack();
+            MineralSupertrumps.turnFinished();
+        });
+        abundance.addActionListener(e -> {
+            MineralSupertrumps.firstTurn = false;
+            MineralSupertrumps.category = 4;
+            remove(categoryChoice);
+            pack();
+            MineralSupertrumps.turnFinished();
+        });
+        ecoValue.addActionListener(e -> {
+            MineralSupertrumps.firstTurn = false;
+            MineralSupertrumps.category = 5;
+            remove(categoryChoice);
+            pack();
+            MineralSupertrumps.turnFinished();
+        });
 
         categoryChoice.add(hardness);
         categoryChoice.add(gravity);
@@ -85,54 +139,84 @@ public class GameFrame extends JFrame implements ActionListener {
         pack();
     }
 
-    void updatePanel(Player player){
+    void updatePanel(Player player) {
+        newRoundNotification.setText("Round " + MineralSupertrumps.roundNumber);
 
-        status.setText("It's player " + player.getID() + " turn");
-
-        int row = player.getHand().size()/4;
-        if (player.getHand().size()%4 != 0){
-            row ++;
+        if (MineralSupertrumps.lastPlayedCard != null) {
+            lastPlayedCard.setIcon(getScaledImage(new ImageIcon("images/" + MineralSupertrumps.lastPlayedCard.getSlideID() + ".jpg")));
+            table.add(lastPlayedCard, "North");
         }
 
-        JPanel hand = new JPanel(new GridLayout(row,DEFAULT_COLUMNS,10,10));
-        hand.setBorder(new EmptyBorder(10,10,10,10));
+
+        deck.setText("Cards left: " + MineralSupertrumps.deck.size());
+        status.setText("It's player " + player.getID() + " turn");
+
+        int row = (player.getHand().size() + 1) / DEFAULT_COLUMNS;
+        if ((player.getHand().size() + 1) % DEFAULT_COLUMNS != 0) {
+            row++;
+        }
+
+        hand = new JPanel(new GridLayout(row, DEFAULT_COLUMNS, 10, 10));
+        hand.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+
+        JButton pass = new JButton("Pass");
+        pass.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                player.passTurn();
+                MineralSupertrumps.turnFinished();
+            }
+        });
+        pass.setFont(new Font("Serif", Font.PLAIN, 20));
+
+
         JButton[] cards = new JButton[player.getHand().size()];
 
+        for (int x = 0; x < player.getHand().size(); x++) {
+            cards[x] = new JButton(getScaledImage(new ImageIcon("images/" + player.getHand().get(x).getSlideID() + ".jpg")));
+            cards[x].setPreferredSize(CARD_SIZE_DIMENSION);
+            cards[x].setText(player.getHand().get(x).getName());
 
-        for (int x = 0; x < player.getHand().size(); x++){
-            cards [x] = new JButton(getScaledImage(new ImageIcon("images/" + player.getHand().get(x).getSlideID() + ".jpg")));
-            cards [x].setPreferredSize(CARD_SIZE_DIMENSION);
-            cards [x].setText(player.getHand().get(x).getName());
+            cards[x].addActionListener(e -> {
+                status.setText("Player " + player.getID() + " has chosen \"" + e.getActionCommand() + "\" card");
 
-            cards [x].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
+                Card chosenCard = null;
+                for (Card card : player.getHand()) {
+                    if (card.getName().equals(e.getActionCommand())) {
+                        chosenCard = card;
+                    }
+                }
+
+                player.removeCard(chosenCard);
+                MineralSupertrumps.lastPlayedCard = chosenCard;
+
+                if (MineralSupertrumps.firstTurn) {
+                    pass.setEnabled(false);
                     for (JButton button :
                             cards) {
                         button.setEnabled(false);
                     }
 
-                    Card chosenCard = null;
-                    for (Card card : player.getHand()) {
-                        if (card.getName().equals(e.getActionCommand())){
-                            chosenCard = card;
-                        }
-                    }
-                    status.setText("Player " + player.getID() + " has chosen \"" + e.getActionCommand() + "\" card");
-                    if (MineralSupertrumps.firstTurn){
-                        categoryPanel(chosenCard);
-                    }
+                    categoryPanel(chosenCard);
+                } else {
+                    MineralSupertrumps.turnFinished();
                 }
             });
 
             hand.add(cards[x]);
         }
-        add(hand,BorderLayout.CENTER);
+
+
+        hand.add(pass);
+
+        add(hand, BorderLayout.CENTER);
+        validate();
+        repaint();
         pack();
     }
-    
 
-    private ImageIcon getScaledImage(ImageIcon imgIcon){
+    private ImageIcon getScaledImage(ImageIcon imgIcon) {
         Image image = imgIcon.getImage();
         Image newImg = image.getScaledInstance(200, 300, Image.SCALE_SMOOTH);
         return new ImageIcon(newImg);
@@ -140,18 +224,18 @@ public class GameFrame extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("3")||e.getActionCommand().equals("4")||e.getActionCommand().equals("5")){
+        if (e.getActionCommand().equals("3") || e.getActionCommand().equals("4") || e.getActionCommand().equals("5")) {
             String number = e.getActionCommand();
-            switch (number){
-                case "3":{
+            switch (number) {
+                case "3": {
                     MineralSupertrumps.setPlayers(3);
                     break;
                 }
-                case "4":{
+                case "4": {
                     MineralSupertrumps.setPlayers(4);
                     break;
                 }
-                case "5":{
+                case "5": {
                     MineralSupertrumps.setPlayers(5);
                     break;
                 }
@@ -161,8 +245,7 @@ public class GameFrame extends JFrame implements ActionListener {
             repaint();
             pack();
             MineralSupertrumps.dealCards();
-            MineralSupertrumps.test();
-            //MineralSupertrumps.runGame();
+            MineralSupertrumps.startGame();
         }
     }
 }
